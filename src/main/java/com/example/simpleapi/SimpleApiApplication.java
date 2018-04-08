3a3970b0cd018091f5d0f0e206ef1c62f77c7a28
Leonardo Henrique <leopr@gmail.com>
2018-04-08 12:05:05 -0300

    first commit

---
+++ src/main/java/com/example/simpleapi/SimpleApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.simpleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApiApplication.class, args);
+	}
+}
